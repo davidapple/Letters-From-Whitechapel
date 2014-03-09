60efba77b39e7eff0b059ef245a7c03d6ab1d041
davidapple <mrdavidapple@gmail.com>
2014-03-09 22:47:30 +0000

    Functions simplified and tidied into object literals, shortest route to base calculated.

--- js/script.js
+++ js/script.js
@@ -8,102 +8,94 @@ character[0].routes = new Array();
 function start(character, mapID) {
 	character.position = mapID;
 	character.routes.push([mapID]);
-	console.log('Start position chosen. Create list of possible move IDs, type possibleMovesByID(true)');
+	console.log('Start position chosen. Create list of possible move IDs, type character[0].routes = move.id(character[0].routes)');
 }
 
-function adjacentIDs() {
-	var counter = 0; // Start counting from zero
-	var newRoutes = new Array(); // Create a new route array
-	for (a = 0; a < character[0].routes.length; a++) { // For every route
-		var lastOne = character[0].routes[a][character[0].routes[a].length - 1] // Find the last position
-		for (b = 0; b < map[lastOne].adjacent.length; b++) { // For every place adjacent to the last one
-			if (isNewID(a, b, lastOne)) { // If the adjacent place is brand new
-				newRoutes[counter] = new Array(); // Create a new route starting from zero
-				for (c = 0; c < character[0].routes[a].length; c++) { // For every place travelled so far
-					newRoutes[counter].push(character[0].routes[a][c]); // Add that to the new route sequentially
+var move = {
+	id: function(r) {
+		var newRoutes = new Array(); // Create a new route array
+		var counter = 0; // Start counting from zero
+		for (a = 0; a < r.length; a++) { // For every route
+			var lastOne = r[a][r[a].length - 1] // Find the last position
+			for (b = 0; b < map[lastOne].adjacent.length; b++) { // For every place adjacent to the last one
+				var p = true; // Assume it's a new place until you check
+				for (c = 0; c < r[a].length; c++) { // For every place traveled so far
+					if (map[lastOne].adjacent[b] == r[a][c]) { // If the next move is the same as a previous one
+						p = false; // Remember that it's not a new place
+					}
 				}
-				newRoutes[counter].push(map[lastOne].adjacent[b]); // Append the adjacent places to the end
-				counter++;
+				if (p) { // If the adjacent place is brand new
+					newRoutes[counter] = new Array(); // Create a new route starting from zero
+					for (c = 0; c < r[a].length; c++) { // For every place travelled so far
+						newRoutes[counter].push(r[a][c]); // Add that to the new route sequentially
+					}
+					newRoutes[counter].push(map[lastOne].adjacent[b]); // Append the adjacent places to the end
+					counter++;
+				}
 			}
 		}
-	}
-	return newRoutes;
-};
-
-function isNewID(a, b, lastOne) { // Test if the next place has been visited already, return true or false
-	var p = true; // Assume it's a new place until you check
-	for (c = 0; c < character[0].routes[a].length; c++) { // For every place traveled so far
-		if (map[lastOne].adjacent[b] == character[0].routes[a][c]) { // If the next move is the same as a previous one
-			p = false; // Remember that it's not a new place
-		}
-	}
-	return p;
-}
-
-function destroyShortRoutes(r) {
-	var counter = 0; // Start counting from zero (how many routes are too short)
-	var longestRoute = r[r.length - 1].length; // Find the length of the last (and therefore longest) route
-
-	for (a = 0; a < r.length; a++) { // For each route
-		if (r[a].length < longestRoute) { // If it's shorter than the longest
-			counter++; // Add one to the counter
-			// r = r.shift() // Annoyingly, this doesn't work
-		}
-	}
-	var desiredRoutes = r.length - counter; // Total routes minus routes that are too short equals number of desired routes
-	var recompileRoutes = new Array(); // Annoyingly, the desired routes need to me recompiled into a new array, shift() doesn't work
-	for (b = 0; b < desiredRoutes; b++) { // For all desired routes
-		recompileRoutes[b] = r.pop(); // Get the last route of the routes array and save it to the start of the recompiled routes array
-	}
-	recompileRoutes.reverse(); // Reverse the routes back into the correct order
-	return recompileRoutes;
-}
-
-function possibleMovesByID() {
-	var tempRoutes = new Array(); // Create a temporary array
-	tempRoutes = adjacentIDs(); // Save new routes to temporary array
-	for (a = 0; a < tempRoutes.length; a++) { // For every new route
-		character[0].routes.push(tempRoutes[a]); // Append it to the master routes list
-	}
-	if (character[0].routes[0] != undefined) { // If routes still exist (added because of error caused by possibleMovesByNumber)
-		character[0].routes = destroyShortRoutes(character[0].routes);
-		console.log('List of possible routes updated: character[0].routes');
-	}
-}
-
-function possibleMovesByNumber() {
-	var numberList = new Array();
-	var adjacentNumbers = new Array();
-	while (character[0].routes[0] != undefined) {
-		possibleMovesByID();
-		var totalRoutes = character[0].routes.length;
-		var spliceList = new Array();
-		for (a = 0; a < totalRoutes; a++) { // For every route
-			if (character[0].routes[a] != undefined) { // If it exists
-				for (b = 1; b < character[0].routes[a].length; b++) { // For every position (ignoring the first one)
-					if (map[character[0].routes[a][b]].number != undefined) { // If it contains a number
+		return newRoutes;
+	},
+	number: function(r) {
+		var newRoutes = new Array(); // Create a new route array
+		var numberList = new Array();
+		while (r[0] != undefined) {
+			r = move.id(r);
+			var totalRoutes = r.length;
+			var spliceList = new Array();
+			for (a = 0; a < totalRoutes; a++) { // For every route
+				if (r[a] != undefined) { // If it exists
+					if (map[r[a][r[a].length - 1]].number != undefined) { // If the last position contains a number
 						var newNumber = true;
 						for (c = 0; c < numberList.length; c++) { // For every number recorded
-							if (numberList[c] == map[character[0].routes[a][b]]) { // If the number has been recorded already
+							if (numberList[c] == map[r[a][r[a].length - 1]]) { // If the number has been recorded already
 								newNumber = false;
 							}
 						}
 						if (newNumber) {
-							numberList.push(map[character[0].routes[a][b]]);
-							console.log(map[character[0].routes[a][b]].number);
-							adjacentNumbers.push(character[0].routes[a]); // Add the route to a list of possible next moves
+							numberList.push(map[r[a][r[a].length - 1]]);
+							newRoutes.push(r[a]); // Add the route to a list of possible next moves
 						}
 						spliceList.push(a);
 					}
 				}
 			}
+			var counter = 0;
+			for (a = 0; a < spliceList.length; a++) { // For every route to splice
+				r.splice(spliceList[a] - counter, 1); // Destroy this route
+				counter++;
+			}
 		}
-		var spliceCounter = 0;
-		for (a = 0; a < spliceList.length; a++) { // For every route to splice
-			character[0].routes.splice(spliceList[a] - spliceCounter, 1); // Destroy this route
-			spliceCounter++;
+		return newRoutes;
+	},
+	base: function(r, base) {
+		var newRoutes = new Array(); // Create a new route array
+		var counter = 0; // Start counting from zero
+		var spliceList = new Array();
+		var totalDesiredRoutes = 20;
+		var routesCounter = 0; // Start counting from zero
+		var isComplete = false;
+		while (!isComplete) {
+			counter++;
+			r = move.number(r);
+			for (a = 0; a < r.length; a++) { // For every route
+				if (r[a] != undefined) { // If it exists
+					if (r[a][r[a].length - 1] == base) { // If the last position is the base
+						console.log('Number of numbers passed = ' + counter);
+						console.log(r[a]);
+						routesCounter++;
+						if (routesCounter > totalDesiredRoutes) {
+							isComplete = true;
+						}
+						spliceList.push(a);
+					}
+				}
+			}
+			var spliceCounter = 0;
+			for (a = 0; a < spliceList.length; a++) { // For every route to splice
+				r.splice(spliceList[a] - spliceCounter, 1); // Destroy this route
+				spliceCounter++;
+			}
 		}
-	}
-	character[0].routes = adjacentNumbers;
-	return adjacentNumbers;
+	},
 }
