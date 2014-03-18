da909c3f8a715b3b13a3b65958a759853b11df6c
davidapple <mrdavidapple@gmail.com>
2014-03-18 23:52:21 +0000

    Content file created, map positions added, style tweaks.

---
+++ js/content.js
@@ -0,0 +1,28 @@
+var state = new Array();
+for (a = 0; a <= 11; a++ ){
+    state[a] = new Array();
+}
+state[0].title = 'Prepare the scene';
+state[0].description = 'Jack collects the special movement tokens';
+state[1].title = 'The targets are identified';
+state[1].description = 'Jack places women tokens facedown on red numbered circles';
+state[2].title = 'Patrolling the streets';
+state[2].description = 'The head of the investigation places the police patrol tokens';
+state[3].title = 'The victims are chosen';
+state[3].description = 'The women tokens marked red are replaced with wretched pawns';
+state[4].title = 'Blood on the streets';
+state[5].description = 'Jack chooses between killing or waiting';
+state[5].title = 'Suspense grows';
+state[5].description = 'The time of the crime token is moved, and each wreched pawn moves';
+state[6].title = 'Ready to kill';
+state[6].description = 'Jack reveals a police patrol token';
+state[7].title = 'A corpse on the sidewalk';
+state[7].description = 'Jack records on his sheet the number of the crime scene';
+state[8].title = 'Alarm whistles';
+state[8].description = 'Replace the marked police patrol tokens with the corresponding pawns';
+state[9].title = 'Escape the night';
+state[9].description = 'Jack moves, records his location, and advances the pawn on the move track';
+state[10].title = 'Hunting the monster';
+state[10].description = 'Each policeman pawn moves';
+state[11].title = 'Clues and suspicion';
+state[11].description = 'Each policeman pawn either looks for clues or executes an arrest';
