8359c826f6b7ec884a2c83836717de5ee51f575b
davidapple <mrdavidapple@gmail.com>
2014-04-09 16:37:42 +0100

    Police stations added, Patrolling The Streets finished, Blood On The Streets started.

--- js/script.js
+++ js/script.js
@@ -15,8 +15,13 @@ var game = {
 		carriages: 3,
 		women: 8,
 		wretched: 4,
+		police: 7,
+		fakePolice: 2,
 		womenMarked: new Array(),
-		womenUnmarked: new Array()
+		womenUnmarked: new Array(),
+		policeMarked: new Array(),
+		policeUnmarked: new Array(),
+		policeRevealed: new Array()
 	},
 	nextState: function() {
 		$('.state').hide();
@@ -31,7 +36,6 @@ var game = {
 			}).prependTo('.prepare-the-scene');
 		}
 		if (game.config.state == 1) { /* The targets are identified */
-			game.config.womenMarked = new Array();
 			$('.the-targets-are-identified').show();
 			$('.the-targets-are-identified .next-state').hide();
 			$('<p></p>', {
@@ -42,13 +46,13 @@ var game = {
 					$('<span></span>', {
 						text: a + ' marked',
 						'data-mapid': a,
-						class: 'label label-info selectable token-woman marked token-woman-' + a,
+						class: 'label label-info selectable token token-woman marked token-woman-' + a,
 						style: 'left:' + map[a].position[0] + ';' + 'top:' + map[a].position[1] + ';'
 					}).appendTo('.map');
 					$('<span></span>', {
 						text: a + ' not marked',
 						'data-mapid': a,
-						class: 'label label-info selectable token-woman unmarked token-woman-' + a,
+						class: 'label label-info selectable token token-woman unmarked token-woman-' + a,
 						style: 'left:' + map[a].position[0] + ';' + 'top:' + map[a].position[1] + ';'
 					}).appendTo('.map');
 				}
@@ -87,6 +91,7 @@ var game = {
 				}
 				if (game.config.womenMarked.length >= (game.config.women - game.config.wretched) && game.config.womenUnmarked.length >= game.config.wretched) {
 					$('.the-targets-are-identified .next-state').show().click(function(){
+						$('.token-woman').remove();
 						game.config.state = 2;
 						game.nextState();
 					});
@@ -96,15 +101,148 @@ var game = {
 			});
 		}
 		if (game.config.state == 2) { /* Patrolling the streets */
-			$('.token-woman').remove();
+			$('.patrolling-the-streets').show();
+			$('.patrolling-the-streets .next-state').hide();
+			$('<p></p>', {
+				text: 'The head of the investigation places' + game.config.police + 'police patrol tokens and ' + game.config.fakePolice + ' fake police tokens on the map.'
+			}).prependTo('.the-targets-are-identified');
 			for (a = 0; a < map.length; a++) {
 				if (($.inArray(a, game.config.womenMarked) !== -1) || ($.inArray(a, game.config.womenUnmarked) !== -1)) {
 					$('<span></span>', {
 						text: 'woman',
 						'data-mapid': a,
-						class: 'label label-info token-woman token-woman-' + a,
+						class: 'label label-info token token-woman token-woman-' + a,
 						style: 'left:' + map[a].position[0] + ';' + 'top:' + map[a].position[1] + ';'
 					}).appendTo('.map');
+				}
+				if (map[a].station) {
+					$('<span></span>', {
+						text: a + ' police',
+						'data-mapid': a,
+						class: 'label label-info selectable token token-police marked token-police-' + a,
+						style: 'left:' + map[a].position[0] + ';' + 'top:' + map[a].position[1] + ';'
+					}).appendTo('.map');
+					$('<span></span>', {
+						text: a + ' not police',
+						'data-mapid': a,
+						class: 'label label-info selectable token token-police unmarked token-police-' + a,
+						style: 'left:' + map[a].position[0] + ';' + 'top:' + map[a].position[1] + ';'
+					}).appendTo('.map');
+				}
+			}
+			$('.token-police').click(function(){
+				var mapid = $(this).data('mapid');
+				if ($(this).hasClass('marked')) {
+					if ($(this).hasClass('selected')) {
+						$(this).removeClass('selected');
+						game.config.policeMarked.splice($.inArray(mapid, game.config.policeMarked), 1);
+					} else {
+						if (game.config.policeMarked.length < (game.config.police - game.config.fakePolice)) {
+							$(this).addClass('selected');
+							game.config.policeMarked.push(mapid);
+							if ($(this).next().hasClass('selected')) {
+								$(this).next().removeClass('selected');
+								game.config.policeUnmarked.splice($.inArray(mapid, game.config.policeUnmarked), 1);
+							}
+						}
+					}
+				}
+				if ($(this).hasClass('unmarked')) {
+					if ($(this).hasClass('selected')) {
+						$(this).removeClass('selected');
+						game.config.policeUnmarked.splice($.inArray(mapid, game.config.policeUnmarked), 1);
+					} else {
+						if (game.config.policeUnmarked.length < game.config.fakePolice) {
+							$(this).addClass('selected');
+							game.config.policeUnmarked.push(mapid);
+							if ($(this).prev().hasClass('selected')) {
+								$(this).prev().removeClass('selected');
+								game.config.policeMarked.splice($.inArray(mapid, game.config.policeMarked), 1);
+							}
+						}
+					}
+				}
+				if (game.config.policeMarked.length >= (game.config.police - game.config.fakePolice) && game.config.policeUnmarked.length >= game.config.fakePolice) {
+					$('.patrolling-the-streets .next-state').show().click(function(){
+						$('.token-woman').remove();
+						$('.token-police').remove();
+						game.config.state = 3;
+						game.nextState();
+					});
+				} else {
+					$('.patrolling-the-streets .next-state').hide();
+				}
+			});
+		}
+		if (game.config.state == 3) { /* Blood on the streets */
+			for (a = 0; a < map.length; a++) {
+				if ($.inArray(a, game.config.womenMarked) !== -1) {
+					$('<span></span>', {
+						text: 'wretched',
+						'data-mapid': a,
+						class: 'label label-info selectable token token-wretched token-wretched-' + a,
+						style: 'left:' + map[a].position[0] + ';' + 'top:' + map[a].position[1] + ';'
+					}).appendTo('.map');
+				}
+				if ($.inArray(a, game.config.policeRevealed) !== -1) {
+					if ($.inArray(a, game.config.policeMarked) !== -1) {
+						$('<span></span>', {
+							text: 'real police',
+							'data-mapid': a,
+							class: 'label label-info revealed token token-police token-police-' + a,
+							style: 'left:' + map[a].position[0] + ';' + 'top:' + map[a].position[1] + ';'
+						}).appendTo('.map');
+					}
+				} else {
+					if (($.inArray(a, game.config.policeMarked) !== -1) || ($.inArray(a, game.config.policeUnmarked) !== -1)) {
+						$('<span></span>', {
+							text: 'police',
+							'data-mapid': a,
+							class: 'label label-info selectable token token-police token-police-' + a,
+							style: 'left:' + map[a].position[0] + ';' + 'top:' + map[a].position[1] + ';'
+						}).appendTo('.map');
+					}
+				}
+			}
+			$('.token-police.selectable').click(function(){
+				var mapid = $(this).data('mapid');
+				if ($.inArray(mapid, game.config.policeUnmarked)) {
+					game.config.policeRevealed.push(mapid);
+					$('.token-wretched').remove();
+					$('.token-police').remove();
+					game.config.state = 4;
+					game.nextState();
+				}
+			});
+		}
+		if (game.config.state == 4) { /* Suspense grows */
+			for (a = 0; a < map.length; a++) {
+				if ($.inArray(a, game.config.womenMarked) !== -1) {
+					$('<span></span>', {
+						text: 'wretched',
+						'data-mapid': a,
+						class: 'label label-info token token-wretched token-wretched-' + a,
+						style: 'left:' + map[a].position[0] + ';' + 'top:' + map[a].position[1] + ';'
+					}).appendTo('.map');
+				}
+				if ($.inArray(a, game.config.policeRevealed) !== -1) {
+					if ($.inArray(a, game.config.policeMarked) !== -1) {
+						$('<span></span>', {
+							text: 'real police',
+							'data-mapid': a,
+							class: 'label label-info revealed token token-police token-police-' + a,
+							style: 'left:' + map[a].position[0] + ';' + 'top:' + map[a].position[1] + ';'
+						}).appendTo('.map');
+					}
+				} else {
+					if (($.inArray(a, game.config.policeMarked) !== -1) || ($.inArray(a, game.config.policeUnmarked) !== -1)) {
+						$('<span></span>', {
+							text: 'police',
+							'data-mapid': a,
+							class: 'label label-info token token-police token-police-' + a,
+							style: 'left:' + map[a].position[0] + ';' + 'top:' + map[a].position[1] + ';'
+						}).appendTo('.map');
+					}
 				}
 			}
 		}
@@ -125,8 +263,9 @@ var draw = {
 						style: 'left:' + map[a].position[0] + ';' + 'top:' + map[a].position[1] + ';'
 					}).prependTo('.map');
 				} else {
+					var station = (map[a].station ? ' location-station' : '');
 					$('<span></span>', {
-						class: 'label label-default location location-' + a,
+						class: 'label label-default location location-' + a + station,
 						text: a,
 						style: 'left:' + map[a].position[0] + ';' + 'top:' + map[a].position[1] + ';'
 					}).prependTo('.map');
